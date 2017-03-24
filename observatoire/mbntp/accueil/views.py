60a1502cdb0cd69de7ea071dacdf8c0dd209fda9
malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2017-03-24 12:26:36 -0400

    Ajout de condition dans le login/logout pour aller au https dans le cas du Manitoba

--- observatoire/mbntp/accueil/views.py
+++ observatoire/mbntp/accueil/views.py
@@ -7,7 +7,6 @@ def isManitoba(http_host):
    return True if 'ntpmb.ca' in http_host else False
     #return True
 
-
 def accueil(request):
     if isManitoba(request.META.get('HTTP_HOST')):
         return render(request,'registration/logged_out.html')
