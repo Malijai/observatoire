6e804902c063f5003a6082bb42ec8b00dbdfacd2
malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2017-04-27 14:46:57 -0400

    Ajout d'un systeme de suivi d'envois

---
+++ observatoire/mbntp/suivi/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+#    url(r'^doclist/$', doclist, name='doclist'),
+ #   url(r'^liste/$', dossier_new, name='dossierslist'),
+]
+
