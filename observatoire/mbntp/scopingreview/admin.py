7b41daaa312d716e61cf8bce6cce6545003edf1d
malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2017-08-15 13:51:03 -0400

    from __future__ import unicode_literals è la place de utf8

--- observatoire/mbntp/scopingreview/admin.py
+++ observatoire/mbntp/scopingreview/admin.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-
+from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Article,Typepub,Typeetude,Typeparticipant,Interception,Originebd,Devis
 
