33f23a708cea743122923a441e2a0257cd8bc4ae
malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2017-08-16 08:38:17 -0400

    Retour a l'ancienne version, le bug d'upload etait du aux réglages du serveur

--- observatoire/mbntp/scopingreview/admin.py
+++ observatoire/mbntp/scopingreview/admin.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+
 from django.contrib import admin
+from django.forms import TextInput, Textarea
 from .models import Article,Typepub,Typeetude,Typeparticipant,Interception,Originebd,Devis
+from django.db import models
 
 class ArticleAdmin(admin.ModelAdmin):
+
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':'100'})},
+        models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
+    }
     fieldsets = [
-        ('Article', {'fields': [('nom', 'annee'), 'titre', 'resumecourt',('originebd','typepub')]}),
-#        ('Article', {'fields': [('nom', 'annee'), 'titre', 'resumecourt','articlefile',('originebd','typepub')]}),
+        ('Article', {'fields': [('nom', 'annee'), 'titre', 'resumecourt','articlefile',('originebd','typepub')]}),
         ('Description de l étude', {'fields': [('interception','interceptiontxt'), 'resume',('typeetude','typeetudetxt'),('devisetude', 'duree', 'region')]}),
-        ('Population', {'fields': [('typeparticipanttxt','comparaisonouinon', 'comparaisontxt'),('nparticipants','agemoyen','typeparticipant')]}),
+        ('Population', {'fields': ['typeparticipanttxt',('comparaisonouinon', 'comparaisontxt'),('nparticipants','agemoyen','typeparticipant')]}),
         ('Résultats', {'fields': ['mesuresresultats', 'tauxmesures', 'analyses']}),
         ('Intervention / Programme', {'fields': [('nomprogramme','tsmreference'), 'interventiontxt','clientprogramme','intervenantprogramme','dureeprogramme', 'conditionprogramme',('echec', 'succes'),'descriptionprogramme']}),
         ('Autres informations', {'fields': ['autresinfos', 'origine']}),
@@ -17,6 +23,7 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ('nom', 'annee', 'titre')
 
     list_filter = ['interception',]
+
 
     def save_model(self, request, obj, form, change):
         obj.save()
